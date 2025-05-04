7fc02a62e1a5c38e86cdeca0823ca3727ca15a58
Florian <45694132+flo-bit@users.noreply.github.com>
2025-05-04 23:58:10 +0200

    fix

--- src/lib/server/client.ts
+++ src/lib/server/client.ts
@@ -4,7 +4,7 @@ import { SessionStore, StateStore } from './storage';
 import { dev } from '$app/environment';
 
 const publicUrl = 'https://bluesky-website-generator.vercel.app';
-const port = 5173;
+const port = 5420;
 const url = dev ? `http://[::1]:${port}` : publicUrl;
 
 const encodeCallbackUrl = encodeURIComponent(`${url}/oauth/callback`);
